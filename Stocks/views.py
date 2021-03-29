0865a22fddad9c5445f163882b7502c246dd0ff7
gerard1801 <gerard_vanderwel@hotmail.com>
2021-03-29 12:14:04 +0200

    yahoo data, hmtl bars

--- Stocks/views.py
+++ Stocks/views.py
@@ -13,45 +13,52 @@ def Home(request):
     total_stock_amount = amount_data.sum()
     #extract all stock tickers
     stock_tickers = stock_data.loc[:, "stock"].tolist()
-    #calculate total stock value
-    total_stock_value = calculate_port_value(amount_data.tolist(), stock_tickers)
-    #buy price data
-    price_data = stock_data.loc[:, "price"]
-    #calculate total buy price
-    total_buy_price = buy_price(amount_data.tolist(), price_data.tolist())
-    #
-    total_profit = total_stock_value - total_buy_price
-    #
-    price_change_day = day_change(amount_data, stock_tickers)
 
-    test = [["AAPL","Apple"],["TSLA", "Tesla"]]
-    testen = [{"ticker":"AAPL", "name":"Apple"},{"ticker":"TSLA", "name":"Tesla"}]
-#{% for tickers in stock_tickers %}
-        #            <tr>
-        #                <td>{{ tickers }}</td>
-         #           </tr>
-          #      {% endfor %}
+    yahoo_data = pull_yahoo_data(stock_tickers)
+    #calculate total stock value
+    total_stock_value = calculate_port_value(amount_data.tolist(), stock_tickers, yahoo_data)
+    #buy price data
+    price_data = stock_data.loc[:, "price"].tolist()
+    #calculate total buy price
+    total_buy_price = buy_price(amount_data.tolist(), price_data)
+    #
+    total_profit = total_stock_value[0] - total_buy_price
+
+    col_names = stock_data.columns.values
+
+    portfolio_table = table_data(col_names, stock_tickers, amount_data.tolist(), price_data)
 
     return render(request, 'Stocks.html', {
-        'test':test,
+        'portfolio_table':portfolio_table,
+        'headers':col_names,
         'stock_tickers':stock_tickers,
         'num_stocks':total_stock_amount,
-        'portfolio_value':round(total_stock_value, 1),
+        'portfolio_value':round(total_stock_value[0], 1),
         'profit':round(total_profit, 1),
-        'day_change':round(price_change_day, 1),
+        'day_change':round(total_stock_value[1], 1),
     })
 
-def calculate_port_value(amount_data, stock_tickers):
-    stock_value = []
-    total_port_value = 0
+def pull_yahoo_data(stock_tickers):
+    yahoo_list = []
     for i in stock_tickers:
-        price_dict = YahooFinancials(i).get_stock_price_data()
-        print(price_dict)
-        price = price_dict.get(i).get('regularMarketPrice')
-        stock_value.append(price)
+        yh = YahooFinancials(i)
+        yahoo_list.append(yh.get_stock_price_data())
+    return yahoo_list
+
+def calculate_port_value(amount_data, stock_tickers, yahoo_data):
+    stock_value = []
+    price_change_list = []
+    total_port_value = 0
+    day_price_change = 0
+    index = 0
+    for i in stock_tickers:
+        stock_value.append(yahoo_data[index].get(i).get('regularMarketPrice'))
+        price_change_list.append(yahoo_data[index].get(i).get('regularMarketChange'))
+        index += 1
     for x in range(len(stock_value)):
         total_port_value += stock_value[x] * amount_data[x]
-    return total_port_value
+        day_price_change += price_change_list[x] * amount_data[x]
+    return [total_port_value, day_price_change]
 
 def buy_price(amount_data, price_data):
     total_buy_price = 0
@@ -59,16 +66,15 @@ def buy_price(amount_data, price_data):
         total_buy_price += amount_data[i] * price_data[i]
     return total_buy_price
 
-def day_change(amount_data, stock_tickers):
-    day_price_change = 0
-    price_change_list = []
-    for i in stock_tickers:
-        price_data = YahooFinancials(i).get_stock_price_data()
-        price_change = price_data.get(i).get('regularMarketChange')
-        price_change_list.append(price_change)
-    for x in range(len(price_change_list)):
-        day_price_change += price_change_list[x] * amount_data[x]
-    return day_price_change
+def table_data(col_names, stock_tickers, amount_data, price_data):
+    portfolio_table = []
+    for i in range(len(stock_tickers)):
+        add_list = []
+        add_list.append(stock_tickers[i])
+        add_list.append(amount_data[i])
+        add_list.append(price_data[i])
+        portfolio_table.append(add_list)
+    return portfolio_table
 
 def Portfolio(request):
     return render(request, 'Portfolio.html')
--- static/javascript/maketab.js
+++ static/javascript/maketab.js
@@ -1,19 +1,23 @@
-function createTable (tableData) {
-  const table = document.createElement('table').appendChild(
+function createTable (tableData, headers) {
+  const table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var row = document.createElement('tr');
+  table.appendChild(thead);
+  thead.appendChild(row);
+  for (var i=0; i<headers.length; i++) {
+      var celhead = document.createElement('th');
+      celhead.innerHTML = headers[i]
+      row.appendChild(celhead)
+  }
+  table.appendChild(
     tableData.reduce((tbody, rowData) => {
       tbody.appendChild(
         rowData.reduce((tr, cellData) => {
           tr.appendChild(
-            document
-              .createElement('td')
-              .appendChild(document.createTextNode(cellData))
-          )
-          return tr
-        }, document.createElement('tr'))
-      )
-      return tbody
-    }, document.createElement('tbody'))
-  )
-
+            document.createElement('td').appendChild(document.createTextNode(cellData)))
+          return tr },
+            document.createElement('tr')))
+      return tbody },
+        document.createElement('tbody')))
   document.body.appendChild(table)
 }
